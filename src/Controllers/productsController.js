3526d3327ffeada262136ef71960d6034b20067c
Vicky-peace <victorpeace47@gmail.com>
2024-03-05 12:03:00 +0300

    product routes complete

--- src/Controllers/productsController.js
+++ src/Controllers/productsController.js
@@ -29,6 +29,129 @@ export const createProduct = async (req,res) =>{
        res.status(500).json({
         status: 'error',
         message:error.message
-       })
+       });
     }
-}
+};
+
+//Get all products
+export const getAllProducts = async (req,res) =>{
+    try{
+        const pool = await sql.connect(config.sql)
+        const result = await pool.request()
+        .query('SELECT * FROM Products')
+        res.status(200).json({
+            status: 'success',
+            products: result.recordset,
+        });
+    } catch(error){
+      console.log(error);
+      res.status(500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+};
+
+
+//Get product byID
+export const getProductById = async (req,res) =>{
+    const {ProductID} = req.params;
+    try{
+        const pool = await sql.connect(config.sql);
+        const result = await pool.request()
+        .input('ProductID', sql.Int, ProductID)
+        .query('SELECT * FROM Products WHERE ProductID = @ProductID')
+
+        if(!result.recordset[0]){
+            res.status(404).json({
+                status: 'error',
+                message: 'Product not found'
+            });
+        } else{
+            res.status(200).json({
+                status: 'success',
+                product: result.recordset[0],
+            });
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            status: 'error',
+            message: error.message,
+        });
+    }
+};
+
+//Update product
+export const updateProduct = async (req,res) =>{
+    const {ProductID} = req.params;
+    const {Name, Description, Price, Stock, UpdatedDate} = req.body;
+
+    try{
+   const pool = await sql.connect(config.sql)
+   const result = await pool.request()
+   .input('ProductID', sql.Int, ProductID)
+   .input('Name',sql.VarChar, Name)
+   .input('Description', sql.VarChar,Description)
+   .input('Price', sql.Decimal,Price)
+   .input('Stock', sql.Int, Stock)
+   .input('UpdatedDate', sql.DateTime,UpdatedDate)
+   .query(
+    `
+    UPDATE Products SET
+    Name = @Name,
+    Description = @Description,
+    Price = @Price,
+    Stock = @Stock,
+    UpdatedDate= @UpdatedDate
+    WHERE ProductID = @ProductID
+    `
+   );
+   if(result.rowsAffected[0] === 1){
+    res.status(200).json({
+        status: 'success',
+        message: 'Product updated successfully',
+    });
+   } else{
+    res.status(404).json({
+        status: 'error',
+        message: 'Product not found',
+    });
+   }
+    } catch(error){
+     console.log(error);
+     res.status(500).json({
+        status: 'error',
+        message: error.message,
+     });
+    }
+};
+
+//Delete a product
+export const deleteProduct = async (req,res) =>{
+    const {ProductID} = req.params;
+    try{
+    const pool = await sql.connect(config.sql);
+    const result = await pool.request()
+    .input('ProductID', sql.Int, ProductID)
+    .query('DELETE FROM Products WHERE ProductID = @ProductID');
+
+    if(result.rowsAffected[0] === 1){
+        res.status(200).json({
+            status: "success",
+            message: 'Product deleted successfully',
+        });
+    }else{
+        res.status(404).json({
+            status: 'error',
+            message: 'Product not found',
+        });
+    }
+    } catch(error){
+         console.log(error)
+         res.status(500).json({
+            status: 'error',
+            message: error.message,
+         });
+    }
+};
--- src/Routes/productsRoutes.js
+++ src/Routes/productsRoutes.js
@@ -1,7 +1,13 @@
-import {createProduct} from '../Controllers/productsController.js'
+import {createProduct, deleteProduct, getAllProducts, getProductById, updateProduct} from '../Controllers/productsController.js'
 
 const productRoutes =(app) =>{
     app.route('/api/products')
     .post(createProduct)
+    .get(getAllProducts)
+
+    app.route('/api/products/:ProductID')
+    .get(getProductById)
+    .put(updateProduct)
+    .delete(deleteProduct)
 }
 export default productRoutes;
