ab2bbac3c0fd94b2388f0f5afc0959df1362fff8
Vicky-peace <victorpeace47@gmail.com>
2024-03-06 12:42:47 +0300

    orders controller complete

--- src/Controllers/ordersController.js
+++ src/Controllers/ordersController.js
@@ -27,3 +27,121 @@ export const createOrder = async (req,res) =>{
         })
     }
 }
+
+
+//Retrieve all orders
+export const getAllOrders = async (req,res) =>{
+    try{
+  const pool = await sql.connect(config.sql);
+  const result = await pool.request()
+  .query('SELECT * FROM Orders')
+  res.status(200).json({
+    status: 'success',
+    orders: result.recordset,
+  })
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    }
+};
+
+
+//Retrieve a single order by ID
+export const getOrder = async(req,res) =>{
+    const {OrderID} = req.params;
+
+    try{
+      const pool = await sql.connect(config.sql)
+      const result = await pool.request()
+      .input('OrderID', sql.Int, OrderID)
+      .query('SELECT * FROM  Orders WHERE OrderID = @OrderID');
+
+      if(result.recordset.length === 0){
+        res.status(404).json({
+            status: 'error',
+            message: 'Order not found'
+        });
+      } else{
+        res.status(200).json({
+            status: 'success',
+            order: result.recordset[0],
+        });
+      }
+
+    } catch(error){
+    console.log(error);
+    res.status(500).json({
+        status: 'error',
+        message: error.message,
+    });
+    }
+};
+
+
+//Update the orders
+export const updateOrders = async(req,res) =>{
+    const {OrderID}= req.params;
+    const {CustomerID,TotalAmount} = req.body;
+    try{
+        const pool = await sql.connect(config.sql);
+        const result= await pool.request()
+        .input('OrderID', sql.Int, OrderID)
+        .input('CustomerID', sql.Int,CustomerID)
+        .input('TotalAmount', sql.Decimal, TotalAmount)
+        .query(
+            `
+            UPDATE Orders
+            SET CustomerID = @CustomerID, TotalAmount = @TotalAmount WHERE OrderID = @OrderID
+            `
+        );
+        if(result.rowsAffected[0] === 0){
+            res.status(404).json({
+                status: 'error',
+                message: 'Order not found',
+            });
+        }else{
+            res.status(200).json({
+                status: 'success',
+                message: 'Order updated successfully'
+            });
+        }
+    }catch(error){
+      console.log(error)
+      res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+      });
+    }
+};
+
+//Delete an order
+export const deleteOrder = async(req,res) =>{
+    const {OrderID} = req.params;
+    try{
+     const pool = await sql.connect(config.sql)
+     const result = await pool.request()
+     .input('OrderID', sql.Int, OrderID)
+     .query('DELETE FROM Orders WHERE OrderID = @OrderID');
+
+     if(result.rowsAffected[0] === 0){
+        res.status(404).json({
+            status: 'error',
+            message: 'Order not found',
+        });
+     }else{
+      res.status(200).json({
+        status: 'success',
+        message: 'Order deleted successfully',
+      })
+     }
+    }catch(error){
+      console.log(error);
+      res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+      })
+    }
+}
--- src/Routes/orderRoutes.js
+++ src/Routes/orderRoutes.js
@@ -1,8 +1,17 @@
-import { createOrder } from "../Controllers/ordersController.js";
+import { createOrder, deleteOrder, getAllOrders, getOrder, updateOrders } from "../Controllers/ordersController.js";
 
 const orderRoutes = (app) =>{
 
      app.route('/api/createOrder')
      .post(createOrder)
+
+     app.route('/api/getAllOrders')
+     .get(getAllOrders)
+
+
+    app.route('/api/:OrderID')
+    .get(getOrder)
+    .put(updateOrders)
+    .delete(deleteOrder)
 }
 export default orderRoutes;
